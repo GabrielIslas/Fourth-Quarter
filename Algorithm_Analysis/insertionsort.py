2ff62550527bafe2fb5fe31ae4e05098e0891161
Gabriel Islas <60411092+Gaborro14@users.noreply.github.com>
2023-01-11 18:29:27 -0600

    fourth quarter started

---
+++ Algorithm_Analysis/insertionsort.py
@@ -0,0 +1,12 @@
+def insert_sort(A):
+    for j in range(1, len(A)):
+        key = A[j]
+        i = j - 1
+        while i >= 0 and A[i] > key:
+            A[i+1] = A[i]
+            i = i - 1
+        A[i+1] = key
+
+arr1 = [5, 4, 3, 2, 1]
+insert_sort(arr1)
+print(arr1)
