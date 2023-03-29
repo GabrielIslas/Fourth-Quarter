f4ee67f22985900ccf0a5608f428356192c91c66
Gaborro14 <60411092+Gaborro14@users.noreply.github.com>
2023-03-29 00:03:39 -0600

    minsumpath print path progress

--- Algorithm_Analysis/U2/minimumsumpath.py
+++ Algorithm_Analysis/U2/minimumsumpath.py
@@ -7,24 +7,22 @@ def min_sum_triangle(triangle: list):
 
     for i in range(len(triangle[triangleRows])):
         sumStorage[i] = triangle[triangleRows][i]
+    print(sumStorage)
     
     for i in range(len(triangle) - 2, -1, -1):
         for j in range(len(triangle[i])):
             sumStorage[j] = triangle[i][j] + min(sumStorage[j], sumStorage[j+1])
             minStorage[j] = min(sumStorage[j], sumStorage[j+1])
-        print(minStorage)
-        
-
-    
-
+        print(sumStorage)
     return sumStorage[0]
-
+# look at the sum storages as a triangle as well to print the path
 
 triangle1 = [[2],
              [3,9],
              [1,6,7]]
 
 triangle2 = [[2],[5,4],[1,4,7],[8,6,9,6]]
+
 
 print(min_sum_triangle(triangle2))
 
